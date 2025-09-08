0cd837885e2fd81c9308470b213525886650f630
46tcampbell <157092336+46tcampbell@users.noreply.github.com>
2025-09-08 17:14:25 -0500

    Create initial 16x16 grid of square divs

--- javascript.js
+++ javascript.js
@@ -1,1 +1,20 @@
+const mainContainer = document.getElementById('main-container');
+mainContainer.style.height = '500px';
+mainContainer.style.width = '500px';
+mainContainer.style.backgroundColor = 'black';
+mainContainer.style.display = 'flex';
+mainContainer.style.flexWrap = 'wrap';
+mainContainer.style.margin = 'auto';
+mainContainer.style.border = 'none';
 
+for (let i = 0; i < 256; i++) {
+  const div = document.createElement('div');
+  div.style.backgroundColor = 'red';
+  div.style.height = '6.25%';
+  div.style.width = '6.25%';
+  div.style.margin = 'none';
+  div.style.border = 'none';
+  mainContainer.appendChild(div);
+}
+
+console.log('Hello World!');
